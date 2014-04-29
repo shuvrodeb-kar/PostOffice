e3861edfa93bdbf19858e6ce47d4ccbaef9e09e5
shuvrodeb-kar <shuvrodeb.kar@enfatico.com>
2014-04-29 18:06:19 +0600

    Add new project

---
+++ PostOffice/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace PostOffice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() != 2)
+                return;
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(ConfigurationManager.AppSettings["BaseAddress"]);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var msg = new { Name = args[0], Message = args[1] };
+            HttpResponseMessage response = client.PostAsJsonAsync("/WebServices/Messaging", JsonConvert.SerializeObject(msg)).Result;            
+        }
+        
+    }
+}
